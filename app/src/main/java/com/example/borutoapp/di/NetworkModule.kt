fc72c50c5b512788a889d9d25c9cdfecaab15e08
Dröge, Thomas <thomasdroege@gmx.de>
2023-12-27 19:48:53 +0100

    added NetworkModule for API Requests

---
+++ app/src/main/java/com/example/borutoapp/di/NetworkModule.kt
@@ -0,0 +1,50 @@
+package com.example.borutoapp.di
+
+import androidx.paging.ExperimentalPagingApi
+import com.example.borutoapp.data.remote.BurotoApi
+import com.example.borutoapp.util.Constants.BASE_URL
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@ExperimentalPagingApi
+@ExperimentalSerializationApi
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
+        val contentType =  "application/json".toMediaType()
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBorutoApi(retrofit: Retrofit): BurotoApi {
+        return retrofit.create(BurotoApi::class.java)
+    }
+}
